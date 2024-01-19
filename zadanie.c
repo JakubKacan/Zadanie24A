29a3ec7f75fea1540e7cdd48115cb24ed45c3cb7
JakubKacan <jakub.kacan@onet.pl>
2024-01-19 10:45:59 +0100

    Pierwsza rewizja kodu.

---
+++ zadanie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+int a;
+int b;
+int c;
+printf("Podaj d³ugoœæ boku a:\n");
+scanf("%d", &a);
+
+printf("Podaj d³ugoœæ boku b:\n");
+scanf("%d", &b);
+
+c = a*b;
+printf("Pole prostok¹ta wynosi: %d", c);
+
+return 0;
+
+
+
+
+
+}
